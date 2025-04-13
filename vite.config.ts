3b2376698543975c813fb3935fb06e0ff08a3b5e
Echa Apriliyanto <apriliyantoecha1@gmail.com>
2025-04-13 19:43:04 +0700

    build: cleanify the build result

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,40 @@
+import fs from 'fs-extra';
 import { defineConfig } from 'vite';
 import path from 'path';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
+function moveHtmlToRoot(): import('vite').Plugin {
+  return {
+    name: 'move-html-to-root',
+    closeBundle: async () => {
+      const from = path.resolve(__dirname, 'dist/src/html/popup.html');
+      const to = path.resolve(__dirname, 'dist/popup.html');
+
+      if (await fs.pathExists(from)) {
+        await fs.move(from, to, { overwrite: true });
+        await fs.remove(path.resolve(__dirname, 'dist/src'));
+      }
+    },
+  };
+}
+
 export default defineConfig(({ mode }) => ({
   publicDir: false,
   plugins: [
+    moveHtmlToRoot(),
     viteStaticCopy({
       targets: [
         { src: 'manifest.json', dest: '' },
-        { src: 'src/html/*', dest: '' },
         { src: 'public/*.png', dest: 'assets/img' },
       ],
     }),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   build: {
     emptyOutDir: true,
     sourcemap: mode === 'development',
     rollupOptions: {
       input: {
-        popup: path.resolve(__dirname, 'src/popup.ts'),
+        popup: path.resolve(__dirname, 'src/html/popup.html'),
         background: path.resolve(__dirname, 'src/background.ts'),
         content: path.resolve(__dirname, 'src/content.ts'),
       },
@@ -32,6 +43,7 @@ export default defineConfig(({ mode }) => ({
         chunkFileNames: 'assets/js/[name]-[hash].js',
         assetFileNames: 'assets/[name].[ext]',
         esModule: true,
+        manualChunks: undefined,
       },
     },
   },
