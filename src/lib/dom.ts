c8fe8c49522f52de1cb430c6fd587f681d3ee78f
Echa Apriliyanto <apriliyantoecha1@gmail.com>
2025-04-15 19:49:38 +0700

    feat: a working selection box overlay

--- src/lib/dom.ts
+++ src/lib/dom.ts
@@ -10,12 +10,9 @@ export function coatTheScreen() {
   const div = document.createElement('div');
   div.id = 'select-and-translate-overlay';
   div.style.position = 'fixed';
-  div.style.top = '0';
-  div.style.bottom = '0';
-  div.style.left = '0';
-  div.style.right = '0';
+  div.style.inset = '0';
   div.style.backgroundColor = 'rgba(0,0,0,0.5)';
-  div.style.zIndex = '9999';
+  div.style.zIndex = '2147483646';
   div.style.pointerEvents = 'auto';
 
   document.body.appendChild(div);
@@ -25,49 +22,49 @@ export function applyEventListener() {
   const div = document.querySelector<HTMLDivElement>(
     '#select-and-translate-overlay'
   );
-  if (div) {
-    const onMouseDown = (e: PointerEvent) => {
-      const xDown = e.clientX;
-      const yDown = e.clientY;
 
-      const selectionBox = document.createElement('div');
-      selectionBox.style.position = 'absolute';
-      selectionBox.style.left = `${xDown}px`;
-      selectionBox.style.top = `${yDown}px`;
-      selectionBox.style.border = `1px dashed white`;
+  if (!div) return;
 
-      div.appendChild(selectionBox);
+  const onMouseDown = (e: PointerEvent) => {
+    e.preventDefault();
+    const xDown = e.clientX;
+    const yDown = e.clientY;
 
-      const onMouseMove = (e: PointerEvent) => {
-        const xMove = e.clientX;
-        const yMove = e.clientY;
-        const width = Math.abs(xMove - xDown);
-        const height = Math.abs(yMove - yDown);
-        const left = Math.min(xMove, xDown);
-        const top = Math.min(yMove, yDown);
+    const selectionBox = document.createElement('div');
+    selectionBox.style.position = 'fixed';
+    selectionBox.style.zIndex = '2147483647';
+    selectionBox.style.boxShadow = '0 0 0 100vw rgba(0,0,0,0.5)';
+    selectionBox.style.outline = '2px dashed white';
 
-        selectionBox.style.left = `${left}px`;
-        selectionBox.style.top = `${top}px`;
-        selectionBox.style.width = `${width}px`;
-        selectionBox.style.height = `${height}px`;
-      };
+    div.style.backgroundColor = 'transparent';
+    div.appendChild(selectionBox);
 
-      const onMouseUp = (e: PointerEvent) => {
-        const xUp = e.clientX;
-        const yUp = e.clientY;
+    const onMouseMove = (e: PointerEvent) => {
+      e.preventDefault();
+      const xMove = e.clientX;
+      const yMove = e.clientY;
+      
+      const width = Math.abs(xMove - xDown);
+      const height = Math.abs(yMove - yDown);
+      const left = Math.min(xMove, xDown);
+      const top = Math.min(yMove, yDown);
 
-        console.log(`DOWN: X-${xDown} Y-${yDown}`);
-        console.log(`UP: X-${xUp} Y-${yUp}`);
-
-        div.removeChild(selectionBox);
-        div.removeEventListener('pointerup', onMouseUp);
-        div.removeEventListener('pointermove', onMouseMove);
-      };
-
-      div.addEventListener('pointermove', onMouseMove);
-      div.addEventListener('pointerup', onMouseUp);
+      selectionBox.style.left = `${left}px`;
+      selectionBox.style.top = `${top}px`;
+      selectionBox.style.width = `${width}px`;
+      selectionBox.style.height = `${height}px`;
     };
 
-    div.addEventListener('pointerdown', onMouseDown);
-  }
+    const onMouseUp = (_: PointerEvent) => {
+      // Cleanup
+      div.removeEventListener('pointerup', onMouseUp);
+      div.removeEventListener('pointermove', onMouseMove);
+      document.body.removeChild(div);
+    };
+
+    div.addEventListener('pointermove', onMouseMove, { passive: false });
+    div.addEventListener('pointerup', onMouseUp);
+  };
+
+  div.addEventListener('pointerdown', onMouseDown, { passive: false });
 }
